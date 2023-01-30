622732125d2d661f34e3857cadaf92afda291eaa
test2023-ml-ai-rpa <123524114+test2023-ml-ai-rpa@users.noreply.github.com>
2023-01-30 07:24:24 +0530

    Update 1a.py

--- 1a.py
+++ 1a.py
@@ -1,32 +1,29 @@
-from random import randint
-# the limit within which random numbers are generated
-TRAIN_SET_LIMIT = 1000
-# to create exactly 100 data items
-TRAIN_SET_COUNT = 100
-# list that contains input and corresponding output
-TRAIN_INPUT = list()
-TRAIN_OUTPUT = list()
-# loop to create 100 data items with three columns each
-for i in range(TRAIN_SET_COUNT):
-    a = randint(0, TRAIN_SET_LIMIT)
-    b = randint(0, TRAIN_SET_LIMIT)
-    c = randint(0, TRAIN_SET_LIMIT)
-# creating the output for each data item
-    op = a + (2 * b) + (3 * c)
-    TRAIN_INPUT.append([a, b, c])
-# adding each output to output list
-    TRAIN_OUTPUT.append(op)
-# Sk-Learn contains the linear regression model
-from sklearn.linear_model import LinearRegression
-# Initialize the linear regression model
-predictor = LinearRegression(n_jobs =-1)
-# Fill the Model with the Data
-predictor.fit(X = TRAIN_INPUT, y = TRAIN_OUTPUT)
-# Random Test data
-X_TEST = [[ 10, 20, 30 ]]
-# Predict the result of X_TEST which holds testing data
-outcome = predictor.predict(X = X_TEST)
-# Predict the coefficients
-coefficients = predictor.coef_
+import csv
+num_att = 6
+a = []
+print("\nA\n")
 
-print('Outcome : {}\nCoefficients : {}'.format(outcome, coefficients))
+with open("enjoysport.csv") as csvfile:
+    reader=csv.reader(csvfile)
+    for row in reader:
+        reader.line_num
+        if(row[0]!="sky"):
+            a.append(row)
+            print(row)
+
+print("HYPO")
+hypothesis = ['0']*num_att
+print(hypothesis)
+
+for j in range(0,num_att):
+    hypothesis[j] = a[0][j]
+
+for i in range(0, len(a)):
+  if a[i][num_att]=='yes':
+      for j in range(0, num_att): 
+          if a[i][j] != hypothesis[j]:
+              hypothesis[j] = '?'
+          else:
+              hypothesis[j] = a[i][j]
+
+print(hypothesis)
